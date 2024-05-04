cb37af9ff281edfa2b59797ebf09e4c7d732a13f
houssemchatibi <houssem.chatibi@gmail.com>
2024-05-04 01:24:57 +0100

    users controller added

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,21 @@ import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
 const app =express();
 const PORT = process.env.PORT || 5000;
+
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 
 app.get("/",(req,res) =>{
     res.send("Server is ready!");
@@ -18,6 +28,7 @@ app.use(express.json({ limit: "5mb" })); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 
 app.listen(PORT, () => {
