5659018511467206fb59901ca58c2cd967e22bc8
houssemchatibi <houssem.chatibi@gmail.com>
2024-05-18 15:53:44 +0100

    Home page UI added

---
+++ frontend/src/components/common/Posts.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { POSTS } from '../../utils/db/dummy';
+import PostSkeleton from '../skeletons/PostSkeleton';
+import Post from './Post';
+
+const Posts = () => {
+
+    const isLoading = false;
+
+    
+  return (
+    <div>
+        {isLoading && (
+				<div className='flex flex-col justify-center'>
+					<PostSkeleton />
+					<PostSkeleton />
+					<PostSkeleton />
+				</div>
+			)}
+
+{!isLoading && POSTS?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
+   { !isLoading && POSTS && (
+    <div>
+ {POSTS.map((post) => (
+     <Post key={post._id} post={post} />
+ ))}
+
+</div>
+) }
+ 
+    </div>
+  )
+}
+
+export default Posts
---
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NotificationPage = () => {
+  return (
+    <div>
+      <div className='flex-[4_4_0] border-l border-r border-gray-700 min-h-screen'>
+      <div className='flex justify-between items-center p-4 border-b border-gray-700'>
+      <p className='font-bold'>Notifications</p>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default NotificationPage
